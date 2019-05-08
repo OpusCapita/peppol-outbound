ccacd84b9783490ab25b54768cef21232ae3aa86
thebilge <ibrahimbilge7@gmail.com>
2019-05-08 10:58:50 +0200

    trying to set a2a cert

--- src/main/java/com/opuscapita/peppol/outbound/sender/business/A2AConfiguration.java
+++ src/main/java/com/opuscapita/peppol/outbound/sender/business/A2AConfiguration.java
@@ -3,16 +3,12 @@ package com.opuscapita.peppol.outbound.sender.business;
 import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
-import org.apache.http.ssl.SSLContexts;
 import org.apache.http.ssl.TrustStrategy;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.web.client.ClientHttpRequestFactorySupplier;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
@@ -37,42 +33,36 @@ public class A2AConfiguration {
     @Value("${a2a.password:''}")
     String password;
 
-//    @Bean
-//    // creates ssl validation disabled request factory
-//    public HttpComponentsClientHttpRequestFactory requestFactory() {
-//        HttpComponentsClientHttpRequestFactory requestFactory = null;
-//        try {
-//            TrustStrategy acceptingTrustStrategy = (X509Certificate[] chain, String authType) -> true;
-//            SSLContext sslContext = org.apache.http.ssl.SSLContexts.custom().loadTrustMaterial(null, acceptingTrustStrategy).build();
-//            SSLConnectionSocketFactory csf = new SSLConnectionSocketFactory(sslContext);
-//            CloseableHttpClient httpClient = HttpClients.custom().setSSLSocketFactory(csf).build();
-//            requestFactory = new HttpComponentsClientHttpRequestFactory();
-//            requestFactory.setHttpClient(httpClient);
-//
-//        } catch (NoSuchAlgorithmException | KeyManagementException | KeyStoreException e) {
-//            logger.error("Failed to disable SSL Cert Validation for A2A Endpoint", e);
-//        }
-//        return requestFactory;
-//    }
-//
-//    @Bean
-//    public RestTemplate a2aRestTemplate(RestTemplateBuilder restTemplateBuilder) {
-//        return restTemplateBuilder.requestFactory(new ClientHttpRequestFactorySupplier()).build();
-//    }
-
     @Bean
     public RestTemplate restTemplate() throws Exception {
-        ClassPathResource classPathResource = new ClassPathResource("a2a.cer");
-        SSLContext sslContext = SSLContexts
-                .custom()
-                .loadTrustMaterial(classPathResource.getFile())
-                .build();
-        final CloseableHttpClient client = HttpClients
-                .custom()
-                .setSSLContext(sslContext)
-                .build();
-        return new RestTemplate(new HttpComponentsClientHttpRequestFactory(client));
+        try {
+            TrustStrategy acceptingTrustStrategy = (X509Certificate[] chain, String authType) -> true;
+            SSLContext sslContext = org.apache.http.ssl.SSLContexts.custom().loadTrustMaterial(null, acceptingTrustStrategy).build();
+            SSLConnectionSocketFactory csf = new SSLConnectionSocketFactory(sslContext);
+            CloseableHttpClient httpClient = HttpClients.custom().setSSLSocketFactory(csf).build();
+            HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
+            requestFactory.setHttpClient(httpClient);
+            return new RestTemplate(requestFactory);
+
+        } catch (NoSuchAlgorithmException | KeyManagementException | KeyStoreException e) {
+            logger.error("Failed to disable SSL Cert Validation for A2A Endpoint", e);
+            throw e;
+        }
     }
+
+//    @Bean
+//    public RestTemplate restTemplate() throws Exception {
+//        ClassPathResource classPathResource = new ClassPathResource("a2a.cer");
+//        SSLContext sslContext = SSLContexts
+//                .custom()
+//                .loadTrustMaterial(classPathResource.getFile())
+//                .build();
+//        final CloseableHttpClient client = HttpClients
+//                .custom()
+//                .setSSLContext(sslContext)
+//                .build();
+//        return new RestTemplate(new HttpComponentsClientHttpRequestFactory(client));
+//    }
 
     String getAuthHeader() {
         byte[] basicAuthValue = (username + ":" + password).getBytes();
